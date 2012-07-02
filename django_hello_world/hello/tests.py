aedb3c071482513bde4ce688743bbb685c80c318
Sergey Papin <papins@gmail.com>
2012-07-02 17:53:38 +0300

    ticket:1 added tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,27 +1,26 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
 class HttpTest(TestCase):
     def test_home(self):
+        fixtures = ['initial_data.json']
+
         c = Client()
-        response = c.get(reverse('home'))
+        # response = c.get(reverse('home'))
+        response = c.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello!')
+
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
+
+        self.assertTrue('user_info' in response.context)
+        self.assertEqual(response.context['user_info'].pk, 1)
+
+        user_info = response.context['user_info']
+        self.assertEqual(user_info.name, 'Sergey')
+        self.assertEqual(user_info.last_name, 'Papin')
+        self.assertEqual(user_info.email, 'papins@gmail.com')
+        self.assertEqual(user_info.jabber, 'papins@gmail.com')
+        self.assertEqual(user_info.skype, 'sergey.papa')
