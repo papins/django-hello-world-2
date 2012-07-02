2b2154451f9484f7eab828edb55c742749e9595e
Sergey Papin <papins@gmail.com>
2012-07-02 18:22:13 +0300

    ticket:1 implemented task goals

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    email = models.EmailField()
+    jabber = models.EmailField()
+    skype = models.CharField(max_length=50)
+    other_contacts = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return "%s %s" % (self.name, self.last_name)
--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,8 +1,9 @@
 from annoying.decorators import render_to
-from django.contrib.auth.models import User
+from django_hello_world.hello.models import UserInfo
+from django.shortcuts import get_object_or_404
 
 
 @render_to('hello/home.html')
 def home(request):
-    users = User.objects.filter()
-    return {'users': users}
+    user_info = get_object_or_404(UserInfo, pk=1)
+    return {'user_info': user_info}
